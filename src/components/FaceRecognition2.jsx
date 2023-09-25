efa482348de8fde0c48d23e88d84399fbec85bac
josebetancourtzam18 <josebetancourtzam18@gmail.com>
2023-09-25 16:32:02 -0400

    actaulizacndo componentes

--- src/components/FaceRecognition2.jsx
+++ src/components/FaceRecognition2.jsx
@@ -4,13 +4,10 @@ import './style/style.css'
 
 function FaceRecognition() {
   const videoRef = useRef(null);
-  const canvasRef = useRef(null); // Referencia al elemento canvas
-  const [cameraOn, setCameraOn] = useState(false); // Estado para controlar si la cámara está encendida o apagada
-
-  // Mantén un estado separado para controlar si el canvas debe mostrarse
+  const canvasRef = useRef(null);
+  const [cameraOn, setCameraOn] = useState(false);
   const [canvasVisible, setCanvasVisible] = useState(false);
 
-  // Definir la función startWebcam antes de su uso
   async function startWebcam() {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({
@@ -19,15 +16,14 @@ function FaceRecognition() {
       });
       if (videoRef.current) {
         videoRef.current.srcObject = stream;
-        setCameraOn(true); // Cambia el estado para indicar que la cámara está encendida
-        setCanvasVisible(true); // Muestra el canvas cuando se enciende la cámara
+        setCameraOn(true);
+        setCanvasVisible(true);
       }
     } catch (error) {
       console.error(error);
     }
   }
 
-  // Definir la función stopWebcam antes de su uso
   async function stopWebcam() {
     const video = videoRef.current;
 
@@ -40,8 +36,8 @@ function FaceRecognition() {
       });
 
       video.srcObject = null;
-      setCameraOn(false); // Cambia el estado para indicar que la cámara está apagada
-      setCanvasVisible(false); // Oculta el canvas cuando se apaga la cámara
+      setCameraOn(false);
+      setCanvasVisible(false);
     }
   }
 
@@ -52,7 +48,6 @@ function FaceRecognition() {
         labels.map(async (label) => {
           const descriptions = [];
           for (let i = 1; i <= 2; i++) {
-            // Usa la ruta correcta hacia la imagen en la carpeta public
             const img = await faceapi.fetchImage(`/labels/${label}/${i}.jpeg`);
             const detections = await faceapi
               .detectSingleFace(img)
@@ -69,16 +64,14 @@ function FaceRecognition() {
       const labeledFaceDescriptors = await getLabeledFaceDescriptions();
       const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
       const video = videoRef.current;
+      const canvas = canvasRef.current;
 
-      if (video) {
+      if (video && canvas) {
         await video.play();
-
-        const canvas = faceapi.createCanvasFromMedia(video);
-        document.body.append(canvas);
         const displaySize = { width: video.width, height: video.height };
         faceapi.matchDimensions(canvas, displaySize);
 
-        setInterval(async () => {
+        const intervalId = setInterval(async () => {
           const detections = await faceapi
             .detectAllFaces(video)
             .withFaceLandmarks()
@@ -89,6 +82,7 @@ function FaceRecognition() {
             displaySize
           );
 
+          // Limpia el canvas antes de dibujar nuevamente
           canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
 
           const results = resizedDetections.map((d) => {
@@ -103,6 +97,9 @@ function FaceRecognition() {
             drawBox.draw(canvas);
           });
         }, 100);
+
+        // Limpia el intervalo cuando se desmonta el componente
+        return () => clearInterval(intervalId);
       }
     }
 
@@ -119,7 +116,6 @@ function FaceRecognition() {
 
     loadModels();
 
-    // Limpia la cámara y el canvas cuando el componente se desmonta
     return () => {
       stopWebcam();
     };
@@ -137,6 +133,9 @@ function FaceRecognition() {
           height="450"
         ></video>
       </div>
+      {canvasVisible && (
+        <canvas ref={canvasRef} style={{ display: "block" }}></canvas>
+      )}
       <div>
         <button onClick={startWebcam} disabled={cameraOn}>
           Encender Cámara
@@ -145,10 +144,6 @@ function FaceRecognition() {
           Apagar Cámara
         </button>
       </div>
-      {/* Use el estado `canvasVisible` para controlar si se muestra el canvas */}
-      {canvasVisible && (
-        <canvas ref={canvasRef} style={{ display: "block" }}></canvas>
-      )}
     </div>
   );
 }
